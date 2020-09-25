253595bd16cc0eb718651b73ef71652d7b05173e
Davy <davycarvalho2017@hotmail.com>
2020-09-25 17:38:44 -0300

    atualizando

---
+++ Uri/uri1022.py
@@ -0,0 +1,12 @@
+N1, N2=(input().split())
+D1, D2=(input().split())
+
+N1=
+N2=
+D1=
+D2=
+
+# Soma=(N1*D2+N2*D1)/(D1*D2)
+# Subtracao=(N1*D2-N2*D1)/(D1*D2)
+# Multiplicacao=(N1*N2)/(D1*D2)
+# Divisao=(N1/D1)/(N2/D2), ou seja (N1*D2)/(N2*D1)
---
+++ Uri/uri1168.py
@@ -0,0 +1,14 @@
+# qtd=int(input())
+# um=2
+# dois=5
+# tres=5
+# quatro=4
+# cinco=5
+# seis=6
+# sete=3
+# oito=7
+# nove=6
+# zero=6
+
+a=12345
+print(a.(strip))
