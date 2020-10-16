211329c42a1d70c52f4efd60e9a3e0ffbc999771
Davy <davycarvalho2017@hotmail.com>
2020-10-15 21:10:29 -0300

    organizando arquivos

---
+++ Exercicios_Entra21_Python/Aula 08 - Atividade de Cad de Pessoas/Finalizado com Dicionario/ex_03.py
@@ -0,0 +1,23 @@
+#--- Exercício 3  - Funções
+#--- Escreva uma função para listar pessoas cadastradas:
+#---    a função deve retornar todas as pessoas cadastradas na função do ex1
+#--- Escreva uma função para exibi uma pessoa específica:
+# a função deve retornar uma pessoa cadastrada na função do ex1 filtrando por id
+
+def listar_pessoas(lista_pessoas:list) -> None:
+
+    for pessoa in lista_pessoas:
+        print(f'''
+        Nome: {pessoa['nome']}
+        Sobrenome: {pessoa['sobrenome']}
+        Idade: {pessoa['idade']}''')
+
+
+def listar_pessoa_especifica(id_usuario:int, lista_pessoas:list) -> None:
+
+    for pessoa in lista_pessoas:
+         if pessoa['id_usuario'] == id_usuario:
+            print(f'''
+    Nome: {pessoa['nome']}
+    Sobrenome: {pessoa['sobrenome']}
+    Idade: {pessoa['idade']}''')
---
+++ Exercicios_Entra21_Python/Aula 08 - Atividade de Cad de Pessoas/Finalizado com Dicionario/ex_05.py
@@ -0,0 +1,43 @@
+#--- Exercício 5  - Funções
+#--- Escreva um programa para cadastro de pessoas e endereços:
+#---o programa deve solicitar os dados de pessoa utilizados na função do ex1
+#---o programa deve solicitar os dados de endereços utilizados na função do ex2
+#---o programa deve passar o id obtido na função do ex1 para a função do ex2
+#---o programa deve mostrar ao final os dados de todos as pessoas cadastradas com seus respectivos endereços utilizando as funções do ex3 e ex4
+
+from ler_dados import ler_dados, ler_endereco
+
+from ex_01 import cadastrar_pessoa
+from ex_02 import cadastrar_endereco
+
+from ex_03 import listar_pessoas, listar_pessoa_especifica
+from ex_04 import listar_enderecos,  listar_endereco_epecifico
+
+lista_pessoas = []
+
+def main():
+
+    while True:
+
+        nome, sobrenome, idade = ler_dados()
+        situacao_cadastro = cadastrar_pessoa(nome, sobrenome, idade, lista_pessoas)
+
+        if type(situacao_cadastro) == int:
+            rua, numero, complemento, bairro, cidade, estado = ler_endereco()
+            cadastrar_endereco(situacao_cadastro, rua, numero, complemento, bairro, cidade, estado, lista_pessoas)
+            
+            x = input('\nPara continuar cadastrando digite Enter, para sair digite 0.\n')
+            if x == '0':
+                break
+        else:
+            print(situacao_cadastro + 'Tente novamente.')
+
+    char = '*'
+    print(f'{char*15} PESSOAS CADASTRADAS {char*15}')
+
+    for pessoa in lista_pessoas:
+        listar_pessoa_especifica(pessoa['id_usuario'], lista_pessoas)
+        listar_endereco_epecifico( pessoa['id_usuario'], lista_pessoas)
+
+
+main()
